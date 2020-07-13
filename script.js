42e39518e1dfce1d619bdc3c3b5dcbae2605a99a
Jason Tilley <jttilley007@yahoo.com>
2020-07-12 21:40:17 -0600

    added more psudo code to style and script files

---
+++ script.js
@@ -0,0 +1,6 @@
+// need array of objects for quesitons and answers and correct answer id
+// need funciton to deduct time if wrong answer
+// need funciton to save initials and score
+// need to set a timer when starting quiz and ending when time is up or quiz is finished
+// need function to display high scores
+// need function to clear high scores.
